590f53f84e1de3bde02313576e3130f771dfa456
Michael Roeslein <michaeljroeslein@gmail.com>
2019-07-13 01:36:12 -0400

    Front end succesfully creates database entry and receives server response with entry details

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default () => {
-  return <div>Hello World</div>;
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post("/api/add", { fullUrl: url }).then(({ data: results }) => {
+      console.log("Heres results! ", results);
+    });
+  };
+
+  const handleChange = e => {
+    setUrl(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter full URL here..."
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
