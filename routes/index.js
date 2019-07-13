9ae3eadb05c0308851326a6cb0c8974b04b98753
Michael Roeslein <michaeljroeslein@gmail.com>
2019-07-13 01:21:03 -0400

    Moved /:code route to API router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const { urls } = require("../controllers");
 
+router.get("/:code", async (req, res) => {
+  const { code } = req.params;
+  const { fullUrl } = await urls.get(code);
+
+  res.redirect(fullUrl);
+});
+
 router.post("/add", async (req, res) => {
   const { fullUrl } = req.body;
   const url = await urls.add(fullUrl);
--- server.js
+++ server.js
@@ -2,21 +2,12 @@ const dotenv = require("dotenv").config();
 const db = require("./database/").init();
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const { urls } = require("./controllers");
-
 const app = express();
 
-app.use(bodyParser.json());
-app.use("/api", require("./routes"));
+app.use(express.json());
 app.use(express.static("client/dist"));
 
-app.get("/:code", async (req, res) => {
-  const { code } = req.params;
-  const { fullUrl } = await urls.get(code);
-
-  res.redirect(fullUrl);
-});
+app.use("/api", require("./routes"));
 
 const PORT = process.env.PORT || 3070;
 
